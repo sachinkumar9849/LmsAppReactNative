9d527e342bd778eb69e05bd03de1bd7cb8aa08d7
sachinmadhwa <kjhasachin@gmail.com>
2024-07-31 23:11:48 +0545

    splash screen create and after 2 second redirect another page add

--- src/screens/tutor/Splash.js
+++ src/screens/tutor/Splash.js
@@ -1,16 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {BG_COLOR, THEME_COLOR} from '../../utils/colors';
+import {useNavigation} from '@react-navigation/native';
 
 const Splash = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('ChooseUserType');
+    }, 2000);
+  }, []);
   return (
-    <View>
-      <Text>
-        Splice
+    <View style={styles.container}>
+      <StatusBar backgroundColor={THEME_COLOR} />
+      <Text
+        style={{
+          fontSize: 44,
+          color: 'white',
+          fontWeight: 'bold',
+          textAlign: 'center',
+        }}>
+        Logo
       </Text>
     </View>
-  )
-}
+  );
+};
 
-export default Splash
+export default Splash;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: THEME_COLOR,
+  },
+});
---
+++ src/utils/colors.js
@@ -0,0 +1,4 @@
+export const THEME_COLOR = "#ff6931"
+export const BUTTON_COLOR = "#000000"
+export const BG_COLOR = "#ffffff"
+export const TEXT_COLOR= "#212121"
